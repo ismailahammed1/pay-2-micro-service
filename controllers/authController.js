1320c7cd30a546d3968d51b2b15e77c363f2b9bc
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-10-16 04:28:35 +0600

    first commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,31 +6,44 @@ const bcrypt = require('bcrypt');
 const register = asyncHandler(async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    // Check if a user with the same username or email already exists
-  
- 
 
-    if (!password) {
-      return res.status(400).json({ error: 'Password is required' });
+    // Check if a user with the same username or email already exists
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'User with the same username or email already exists' });
     }
+
     // Generate a salt and hash the password synchronously
-    const saltRounds = 10; // Adjust this according to security requirements
+    const saltRounds = 10; // Adjust this according to your security requirements
     const hashedPassword = bcrypt.hashSync(password, saltRounds);
 
     const newUser = new User({
-      ...req.body,
+      username,
+      email,
       password: hashedPassword,
+      // Include other fields here
     });
+
     await newUser.save();
     res.status(201).send("User has been created.");
   } catch (error) {
-    console.error('Error:', error);
+
     res.status(500).send('Something went wrong');
   }
 });
 
 const login = asyncHandler(async (req, res) => {
-  
+  try {
+    const user=await User.findOne({username:req.body.username});
+    if (!user) return res.status(404).send("user not fund!")
+    const isCorrect=bcrypt.compare(req.body.password, user.password);
+    if (!isCorrect) return res.status(401).send("wrong password or username");
+    const {password, ...info}=user._doc;
+    res.status(200).send(info)
+  } catch (error) {
+    res.status(500).send('Something went wrong');
+  }
   });
   
 const logout = asyncHandler(async (req, res) => {
