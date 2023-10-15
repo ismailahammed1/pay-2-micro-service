e68cc826129aaba3ff01812a5c1d7b709f0098e6
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-10-16 02:43:19 +0600

    Remove node_modules from tracking

--- index.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const dbConnect = require("./config/dbConnect");
-const dotenv = require('dotenv').config();
-const app = express();
-
-dbConnect();
-app.listen(8800, ()=>{
-    console.log("backend server is running");
-})
